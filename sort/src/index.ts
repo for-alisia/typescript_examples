21e18bf353672cd1036cc0eaf9a130781338bfa6
for-alisia <for-alisia@yandex.ru>
2020-09-02 10:21:51 +0500

    Added sort project

---
+++ sort/src/index.ts
@@ -0,0 +1,21 @@
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort() {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([2, 5, 1, -8, 0, 12, 3, -2]);
+sorter.sort();
+console.log(sorter.collection);
