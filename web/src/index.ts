707a944449a3fedb92b2b66677e9df26c76ac92e
for-alisia <for-alisia@yandex.ru>
2021-01-18 13:23:39 +0500

    Added json-server and axios to fetch and save data

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,21 +1,5 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'Alex', age: 8 });
+const user = new User({ name: 'John', age: 27 });
 
-user.set({ name: 'Alice' });
-
-user.on('change', () => {
-  console.log('Change #1');
-});
-
-user.on('change', () => {
-  console.log('change #2');
-});
-
-user.on('save', () => {
-  console.log('save event!!!');
-});
-
-user.trigger('noExistEvent');
-user.trigger('change');
-user.trigger('save');
+user.save();
