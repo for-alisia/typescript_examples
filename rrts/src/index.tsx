b352dee25e3118de82c42b7b8ed944f1437e23e9
for-alisia <for-alisia@yandex.ru>
2021-01-22 11:19:51 +0500

    Added reducers and actions

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,14 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-interface AppProps {
-  color: string;
-}
+import { App } from './components/App';
 
-class App extends React.Component<AppProps> {
-  render() {
-    return <div>{this.props.color}</div>;
-  }
-}
+import { reducers } from './reducers';
 
-ReactDOM.render(<App color='red' />, document.getElementById('root'));
+const store = createStore(reducers, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+// interface AppProps {
+//   color?: string;
+// }
+
+// interface AppState {
+//   counter: number;
+// }
+
+/** Case of functional component */
+// const App = (props: AppProps): JSX.Element => {
+//   return <div>{props.color}</div>;
+// };
+
+/** Case os the simpliest class component */
+// class App extends React.Component<AppProps, AppState> {
+//   // state = { counter: 0 } - in this case we don't need AppState Interface
+
+//   constructor(props: AppProps) {
+//     super(props);
+
+//     this.state = { counter: 0 };
+//   }
+
+//   onIncrement = (): void => {
+//     this.setState({ counter: this.state.counter + 1 });
+//   };
+
+//   onDecrement = (): void => {
+//     this.setState({ counter: this.state.counter - 1 });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <h3>{this.state.counter}</h3>
+//         <button onClick={this.onIncrement}>Increment</button>
+//         <button onClick={this.onDecrement}>Decrement</button>
+//       </div>
+//     );
+//   }
+// }
